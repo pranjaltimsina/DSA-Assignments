028bf63270d90e6d166e0388fc4589863e15a02c
Pranjal Timsina <pranjal.timsina@live.com>
2021-06-08 16:42:50 +0545

    feat: progress

--- Assignment 5/src/BFS.cpp
+++ Assignment 5/src/BFS.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 
 std::vector<std::vector<int>> get_matrix(int points) {
     std::vector<std::vector<int>> adjacency_matrix;
@@ -16,7 +17,45 @@ std::vector<std::vector<int>> get_matrix(int points) {
         }
         adjacency_matrix.push_back(temp);
     }
-    retirm adjacency_matrix;
+    return adjacency_matrix;
+
+}
+
+std::vector<std::vector<int>> get_empty_tree(int points) {
+     std::vector<std::vector<int>> tree;
+     std::vector<int> temp;
+
+    for (int i = 0; i < points; i++) {
+        for (int j = 0; j < points; j++) {
+            temp.push_back(0);
+        }
+        tree.push_back(temp);
+    }
+    return tree;
+}
+
+
+std::vector<std::vector<int>> BFS(std::vector<std::vector<int>> matrix, int points) {
+    std::vector<std::vector<int>> tree = get_empty_tree(points);
+    std::vector<int> explored_points;
+    std::queue<int> frontier;
+
+    frontier.push(0);
+
+    int current;
+
+    while (!frontier.empty()) {
+        current = frontier.front();
+        frontier.pop();
+        explored_points.push_back(current);
+        std::vector<int> connected = matrix.at(current);
+        for (int i = 0; i < points; i++) {
+            if (connected.at(i) == 1 && i != current) {
+                frontier.push(i);
+            }
+        }
+    }
+
 
 }
 
@@ -26,5 +65,7 @@ int main() {
     std::cin >> points;
 
     std::vector<std::vector<int>> adjacency_matrix = get_matrix(points);
+    std::vector<std::vector<int>> tree = BFS(adjacency_matrix, points);
+
 
 }
